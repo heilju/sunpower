1757af68929a7b693140e1d56e9a663d6854ccd2
Juergen Heil <juergen@meema.org>
2015-01-11 11:56:00 +0100

    navbar and footer added
    header div and footer added to default layout

---
+++ app/views/inverter/graph.blade.php
@@ -0,0 +1,151 @@
+@extends('layouts.default')
+@section('content')
+<div class="container">
+    <div class="header">
+        <nav>
+            <ul class="nav nav-pills pull-right">
+                <li role="presentation">{{ HTML::link('./inverter', 'Overview')}}</li>
+                <li role="presentation">{{ HTML::link('#', 'Graphs')}}</li>
+                <li role="presentation">{{ HTML::link('./inverter/create', 'Create')}}</li>
+            </ul>
+        </nav>
+        <h3 class="text-muted">Sonnenkraftwerk Weindlweg 16</h3>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="panel panel-default">
+                <!-- Default panel contents -->
+                <div class="panel-heading"><strong>Leistung</strong></div>
+
+                <!-- Current output value -->
+                <p class="text-center mee-output-current">{{ $inverterValues['acOutputPowerTotal'] }} W</p>
+                <!-- Table -->
+                <table class="table">
+                    <tr>
+                        <td>Gesamt</td>
+                        <td>Tag</td>
+                        <td>Woche</td>
+                        <td>Monat</td>
+                        <td>Jahr</td>
+                    </tr>
+                    <tr>
+                        <td>{{ $inverterValues['acOutputPowerTotalMax'] }} W</td>
+                        <td>{{ $inverterValues['acOutputPowerTotalMaxDay'] }}  W</td>
+                        <td>{{ $inverterValues['acOutputPowerTotalMaxWeek'] }} W</td>
+                        <td>{{ $inverterValues['acOutputPowerTotalMaxMonth'] }} W</td>
+                        <td>{{ $inverterValues['acOutputPowerTotalMaxYear'] }} W</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <!-- DC output -->
+        <div class="col-lg-6">
+            <div class="panel panel-default">
+                <div class="panel-heading"><strong>Tagesenergie</strong></div>
+                <p class="text-center mee-output-daily">{{ $inverterValues['acOutputEnergyDaily'] }} kWh</p>
+                <table class="table">
+                    <tr>
+                        <td>Gesamt</td>
+                        <td>Woche</td>
+                        <td>Monat</td>
+                        <td>Jahr</td>
+                    </tr>
+                    <tr>
+                        <td>{{ $inverterValues['acOutputEnergyDailyMax'] }} kWh</td>
+                        <td>{{ $inverterValues['acOutputEnergyDailyMaxWeek'] }}  kWh</td>
+                        <td>{{ $inverterValues['acOutputEnergyDailyMaxMonth'] }}  kWh</td>
+                        <td>{{ $inverterValues['acOutputEnergyDailyMaxYear'] }}  kWh</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+        <!-- AC Output-->
+        <div class="col-lg-6">
+            <div class="panel panel-default">
+                <div class="panel-heading"><strong>Gesamtenergie</strong></div>
+                <p class="text-center mee-output-total">{{ $inverterValues['acOutputEnergyTotal'] }} kWh</p>
+                <table class="table">
+                    <tr>
+                        <td>Sekunden</td>
+                        <td>Minuten</td>
+                        <td>Stunden</td>
+                        <td>Tage</td>
+                    </tr>
+                    <tr>
+                        <td>6.887</td>
+                        <td>887</td>
+                        <td>119</td>
+                        <td>6</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <!-- DC output -->
+        <div class="col-lg-6">
+            <div class="panel panel-default">
+                <div class="panel-heading"><strong>DC Input</strong></div>
+
+                <table class="table">
+                    <tr>
+                        <td>String</td>
+                        <td>Spannung</td>
+                        <td>Strom</td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>{{ $inverterValues['dcInputVoltageS1'] }} V</td>
+                        <td>{{ $inverterValues['dcInputCurrentS1'] }} A</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>{{ $inverterValues['dcInputVoltageS2'] }} V</td>
+                        <td>{{ $inverterValues['dcInputCurrentS2'] }} W</td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>{{ $inverterValues['dcInputVoltageS3'] }} V</td>
+                        <td>{{ $inverterValues['dcInputCurrentS3'] }} W</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+        <!-- AC Output-->
+        <div class="col-lg-6">
+            <div class="panel panel-default">
+                <div class="panel-heading"><strong>AC Output</strong></div>
+                <table class="table">
+                    <tr>
+                        <td>Phase</td>
+                        <td>Spannung</td>
+                        <td>Leistung</td>
+                    </tr>
+                    <tr>
+                        <td>L1</td>
+                        <td>{{ $inverterValues['acOutputVoltageL1'] }} V</td>
+                        <td>{{ $inverterValues['acOutputPowerL1'] }} W</td>
+                    </tr>
+                    <tr>
+                        <td>L2</td>
+                        <td>{{ $inverterValues['acOutputVoltageL2'] }} V</td>
+                        <td>{{ $inverterValues['acOutputPowerL2'] }} W</td>
+                    </tr>
+                    <tr>
+                        <td>L3</td>
+                        <td>{{ $inverterValues['acOutputVoltageL3'] }} V</td>
+                        <td>{{ $inverterValues['acOutputPowerL3'] }} W</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+    <footer class="footer">
+        <p>&copy; meema.org 2014</p>
+    </footer>
+
+</div>
+@stop
--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -11,9 +11,25 @@
 
 </head>
 <body>
+<div class="container">
+    <div class="header">
+        <nav>
+            <ul class="nav nav-pills pull-right">
+                <li role="presentation" class="active"><a href="http://d-sunpower.meema.lan/inverter">Overview</a></li>
+                <li role="presentation"><a href="http://d-sunpower.meema.lan/inverter/create">Create</a></li>
+                <li role="presentation"><a href="http://d-sunpower.meema.lan/inverter/graph">Graphs</a></li>
+            </ul>
+        </nav>
+        <h3 class="text-muted">Sunpower - Weindlweg 16</h3>
+    </div>
 
 @yield('content')
 
+    <footer class="footer">
+        <p>&copy; meema.org 2014</p>
+    </footer>
+
+</div>
 <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
 <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
